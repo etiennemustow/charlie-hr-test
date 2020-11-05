94a45bab42e9eab0aad398871301f2f36629a41c
Etienne Mustow <etienne.o.mustow@gmail.com>
2020-11-05 20:34:36 +0000

    added more tests to test suite and added instructions readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# To setup
+
+### Enter this command in the terminal 
+
+``` bundle install ```
+
+# To run tests
+
+### Run this command in terminal to run rspec tests
+
+```rspec```
+
+### Run this command in terminal to run test suite
+
+``` ruby tests.rb ```
+
--- tests.rb
+++ tests.rb
@@ -3,7 +3,13 @@ require_relative 'user.rb'
   tests = [
     Date.new(1986, 1, 1),
     Date.new(1988, Date.today.month, Date.today.day),
+    Date.new(2000, 6, 21),
+    Date.new(1963, 1, 10),
+    Date.new(1969, 5, 6),
+    Date.new(2003, 3, 12),
     Date.new(1990, 12, 30),
+    Date.new(1997, 7, 7),
+    Date.new(1923, 4, 15),
     ]
     puts "====== ages ======"
     tests.each do |date|
